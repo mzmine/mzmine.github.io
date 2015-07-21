f5d26d4b7a309098c2bd0ce21a005006f8548cd0
Thomas F. Dyrlund <thomas@dyrlund.dk>
2015-07-21 13:50:04 +0200

    Finished the features page

--- js/custom.js
+++ js/custom.js
@@ -29,3 +29,93 @@ $(document).ready(function() {
 		}
 	});
 });
+$(document).ready(function() {
+	$('#imgGallery_2').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1]
+		},
+		image: {
+		  markup: '<div class="mfp-figure mfp-figure-custom">'+
+					'<div class="modal-header">'+
+					  '<div class="mfp-close"></div>'+
+					  '<div class="mfp-bottom-bar">'+
+					    '<div class="mfp-title"></div>'+
+						//'<div class="mfp-counter"></div>'+
+					  '</div>'+
+					'</div>'+
+					'<div class="mfp-img"></div>'+
+				  '</div>',
+
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		}
+	});
+});
+$(document).ready(function() {
+	$('#imgGallery_3').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1]
+		},
+		image: {
+		  markup: '<div class="mfp-figure mfp-figure-custom">'+
+					'<div class="modal-header">'+
+					  '<div class="mfp-close"></div>'+
+					  '<div class="mfp-bottom-bar">'+
+					    '<div class="mfp-title"></div>'+
+						//'<div class="mfp-counter"></div>'+
+					  '</div>'+
+					'</div>'+
+					'<div class="mfp-img"></div>'+
+				  '</div>',
+
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		}
+	});
+});
+$(document).ready(function() {
+	$('#imgGallery_4').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1]
+		},
+		image: {
+		  markup: '<div class="mfp-figure mfp-figure-custom">'+
+					'<div class="modal-header">'+
+					  '<div class="mfp-close"></div>'+
+					  '<div class="mfp-bottom-bar">'+
+					    '<div class="mfp-title"></div>'+
+						//'<div class="mfp-counter"></div>'+
+					  '</div>'+
+					'</div>'+
+					'<div class="mfp-img"></div>'+
+				  '</div>',
+
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		}
+	});
+});
